d540338029a1a40be313877251ee119cf016274c
ReinisKalnarajs <reinis.kalnarajs@gmail.com>
2021-02-02 13:41:19 +0200

    Initial Commit

---
+++ DayFinderV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayFinderV2
+//
+//  Created by reinis.k.kalnarajs on 02/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
